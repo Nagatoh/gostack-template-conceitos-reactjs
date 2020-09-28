bff9c38f61b29cfa288aa040221bcc68c69867eb
Nagato <humberto.nagatog@gmail.com>
2020-09-27 22:30:07 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setProjects(response.data);
+      // console.log(response);
+    });
+  }, []);
+
+
   async function handleAddRepository() {
-    // TODO
+    //projects.push(`Novo projeto ${Date.now()}`);
+    //"..."=>spreadOperator recria o vetor com as mesmas informações
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    const response = await api.post('repositories', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Humberto Nagato'
+    });
+    const project = response.data
+    setProjects([...projects, project]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    setProjects(projects.filter(project=>project.id!==id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {projects.map(project =>
+          <li key={project.id}>
+            {project.title}
+            <button onClick={() => handleRemoveRepository(project.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
